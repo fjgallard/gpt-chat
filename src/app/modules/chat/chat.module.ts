c33886b03d00b56b8437d40a451bef3383491929
Francis Gallardo <francis.gallardo@globe.com.ph>
2023-08-21 13:35:38 +0800

    Setup basic page layout.

--- src/app/modules/chat/chat.module.ts
+++ src/app/modules/chat/chat.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ChatRoutingModule } from './chat-routing.module';
 import { ChatComponent } from './chat.component';
+import { HistoryComponent } from './components/history/history.component';
+import { ConversationComponent } from './components/conversation/conversation.component';
 
 
 @NgModule({
   declarations: [
-    ChatComponent
+    ChatComponent,
+    HistoryComponent,
+    ConversationComponent,
   ],
   imports: [
     CommonModule,
